c5bb8eef6821a23ed5a26102cedd56c5a983cf60
kodeck Nyangeri Otiso <kodeckotiso@gmail.com>
2023-05-22 14:42:15 +0300

    my kenyan tender convertor in KSHS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {React,useState,useRef} from 'react';
 
 function App() {
+  const inputRef = useRef(null);
+  const [usdPrice, setusdPrice] = useState(0);
+  const [results, setResults] = useState(0);
+
+  function handleChange(e)
+  {
+  setusdPrice(e.target.value);
+  };
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    setResults ((results) => {
+      const add = results + Number(inputRef.current.value) + 50;
+      return <li>{add}</li>
+      
+    });
+  
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <fieldset>
+      <h1>USD: {usdPrice} </h1>
+      <h1>KSHS:{results}</h1>
+      <ul>
+        {handleSubmit}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="number" ref={inputRef} onChange={handleChange}V ></input>
+        <button type="submit">Convert</button>
+      </form>
+      </fieldset>   
+     </div>
   );
-}
-
+};
 export default App;
